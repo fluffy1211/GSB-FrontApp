4673f0426fb96cfb01f9f6a39308cff327990f50
Zacdx <cyanzac@gmail.com>
2025-01-20 17:09:54 +0100

    navbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:gsb/navbar.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const SearchBarApp());
 }
+
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
